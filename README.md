bc5b1eb69baf2b7c758a0a13928fa28760a4a158
Scotty Logan <swl@stanford.edu>
2017-08-30 15:32:37 -0700

    major updates

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-Simple Terraform module to create DNS records for
-Kerberos
+Simple Terraform module to create DNS records for Kerberos
+
+
+## Usage
+
+Minimum configuration:
+
+    module "example_krb5" {
+      source  = "github.com/scottylogan/route53-kerberos"
+      zone_id = "${aws_route53_zone.example.zone_id}"
+    }
+
+This would create records in the _example.com_ domain for the _EXAMPLE.COM_ Kerberos realm:
+
+    _kerberos.example.com.             300 IN TXT "EXAMPLE.COM"
+
+    _kerberos._udp.example.com.        300 IN SRV 0 0  88 kdc-1.example.com.
+                                                  0 0  88 kdc-2.example.com.
+                                                  0 0  88 kdc-3.example.com.
+
+    _kerberos._tcp.example.com.        300 IN SRV 0 0  88 kdc-1.example.com.
+                                                  0 0  88 kdc-2.example.com.
+                                                  0 0  88 kdc-3.example.com.
+
+    _kerberos-master._udp.example.com. 300 IN SRV 0 0  88 kdc-1.example.com.
+
+    _kerberos-adm._tcp.example.com.    300 IN SRV 0 0 749 kdc-1.example.com.
+
+    _kpasswd._udp.example.com.         300 IN SRV 0 0 464 kdc-1.example.com.
+
+## Variables
+
+### zone_id
+
+ID of AWS Route53 Hosted Zone to update (*REQUIRED*). The domain name
+is retrieved from the hosted zone definition.
+
+### realm
+
+Name of the Kerberos Realm. Defaults to the domain name in upper case.
+
+### prefix
+
+Prefix to be used to create KDC server names for SRV record targets.  Defaults to _kdc_.
+
+### master_name
+
+Name of the master KDC. Defaults to the first KDC (_kdc-1.DOMAIN_)
+
+### ttl
+
+Defines the DNS record TTL (in seconds). Defaults to _300_.
+
+### kdcs
+
+Number of KDC targets to create for the SRV records. Defaults to the
+number of availability zones in the current AWS region.
+
+### kpasswd
+
+Controls the creation of the `_kpasswd._udp` SRV record. Defaults to _true_.
+
+### kadmin
+
+Controls the creation of the `_kerberos-adm._tcp` SRV record. Defaults to _true_.
+
+
+## Configuration Example
+
+For historical reasons, Stanford has used a lower-case Kerberos 5 realm
+name. Also, since our IT Lab development domain is used for development
+and testing, we don't allow password changes via `kpasswd`, and we want
+shorter DNS TTLs:
+
+    module "dev_kerberos" {
+      source  = "github.com/scottylogan/route53-kerberos"
+      zone_id = "${aws_route53_zone.dev.zone_id}"
+      realm   = "dev.itlab.stanford.edu"
+      ttl     = "30"
+      kpasswd = false
+    }
+
--- records.tf
+++ records.tf
@@ -1,25 +1,30 @@
 data "aws_availability_zones" "azs" {}
 
+data "aws_route53_zone" "krb5" {
+  zone_id = "${var.zone_id}"
+}
+
 variable "zone_id" {
   type = "string"
-  description = "Route53 ZoneID"
+  description = "AWS Route53 ZoneID"
 }
 
-variable "domain" {
+variable "prefix" {
   type = "string"
-  description = "Domain Name"
-}
-
-variable "kdc_name" {
-  type = "string"
-  description = "kdc name prefix"
+  description = "Name prefix for KDC targets in SRV records"
   default = "kdc"
 }
 
-variable "kdc_master_name" {
+variable "master_name" {
   type = "string"
   description = "master KDC name"
-  default = "kdc-master"
+  default = ""
+}
+
+variable "realm" {
+  type = "string"
+  description = "Kerberos realm name"
+  default = ""
 }
 
 variable "ttl" {
@@ -28,69 +33,96 @@ variable "ttl" {
   description = "DNS Record TTL"
 }
 
-variable "kdc_srv_records" {
-  type = "list"
-  default = "${formatlist("0 0 88 ${var.kdc_name}-%s.%s", ${data.aws_availability_zones.available.names}, ${var.domain})}"
+variable "kdcs" {
+  type = "string"
+  default = "0"
+  description = "Number of KDC targets to create for SRV records"
+}
+
+variable "kpasswd" {
+  default = true
+  description = "Control the creation of the _kpasswd._udp SRV record"
+}
+
+variable "kadmin" {
+  default = true
+  description = "Control the creation of the _kerberos-adm._tcp SRV record"
 }
 
 resource "aws_route53_record" "krb5-txt" {
   zone_id = "${var.zone_id}"
-  name    = "_kerberos.${var.domain}"
+#  name    = "_kerberos.${var.domain}"
+  name    = "_kerberos.${data.aws_route53_zone.krb5.name}"
   type    = "TXT"
   ttl     = "${var.ttl}"
   
   records = [
-    "${var.domain}"
+    "${var.realm == "" ? upper(data.aws_route53_zone.krb5.name) : var.realm}"
   ]
+}
+
+data "template_file" "kdc_records" {
+  count = "${var.kdcs == "0" ? length(data.aws_availability_zones.azs.names) : var.kdcs}"
+  template = "0 0 88 $${kdc_name}"
+  vars {
+    kdc_name = "${format("%s-%d.%s", var.prefix, count.index + 1, data.aws_route53_zone.krb5.name)}"
+  }
+}
+
+data "template_file" "master_name" {
+  template = "$${kdc_name}"
+  vars {
+    kdc_name = "${format("%s.%s", var.master_name == "" ? format("%s-1", var.prefix) : var.master_name, data.aws_route53_zone.krb5.name)}"
+  }
 }
 
 resource "aws_route53_record" "krb5-tcpsrv" {
   zone_id = "${var.zone_id}"
-  name    = "_kerberos._tcp.${var.domain}"
+  name    = "_kerberos._tcp.${data.aws_route53_zone.krb5.name}"
   type    = "SRV"
   ttl     = "${var.ttl}"
-  
-  records = "${kdc_srv_records}"
+  records = ["${data.template_file.kdc_records.*.rendered}"]
 }
 
 resource "aws_route53_record" "krb5-udpsrv" {
   zone_id = "${var.zone_id}"
-  name    = "_kerberos._udp.${var.domain}"
+  name    = "_kerberos._udp.${data.aws_route53_zone.krb5.name}"
   type    = "SRV"
   ttl     = "${var.ttl}"
-  
-  records = "${kdc_srv_records}"
+  records = ["${data.template_file.kdc_records.*.rendered}"]
 }
 
 resource "aws_route53_record" "krb5-master-udpsrv" {
   zone_id = "${var.zone_id}"
-  name    = "_kerberos-master._udp.${var.domain}"
+  name    = "_kerberos-master._udp.${data.aws_route53_zone.krb5.name}"
   type    = "SRV"
   ttl     = "${var.ttl}"
   
   records = [
-    "0 0 88 ${var.kdc_master_name}.${var.domain}."
+    "0 0 88 ${data.template_file.master_name.rendered}"
   ]
 }
 
 resource "aws_route53_record" "krb5-adm-tcpsrv" {
+  count   = "${var.kadmin ? 1 : 0}"
   zone_id = "${var.zone_id}"
-  name    = "_kerberos-adm._tcp.${var.domain}"
+  name    = "_kerberos-adm._tcp.${data.aws_route53_zone.krb5.name}"
   type    = "SRV"
   ttl     = "${var.ttl}"
   
   records = [
-    "0 0 749 ${var.kdc_master_name}.${var.domain}."
+    "0 0 749 ${data.template_file.master_name.rendered}"
   ]
 }
 
 resource "aws_route53_record" "krb5-kpasswd-udpsrv" {
+  count   = "${var.kpasswd ? 1 : 0}"
   zone_id = "${var.zone_id}"
-  name    = "_kpasswd._udp.${var.domain}"
+  name    = "_kpasswd._udp.${data.aws_route53_zone.krb5.name}"
   type    = "SRV"
   ttl     = "${var.ttl}"
   
   records = [
-    "0 0 464 ${var.kdc_master_name}.${var.domain}."
+    "0 0 464 ${data.template_file.master_name.rendered}"
   ]
 }
